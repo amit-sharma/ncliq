d52f83fa451c8c74095725a67d8fc3bb539f319e
Sudip Roy <sroyiitkgp@gmail.com>
2012-10-20 21:45:57 -0400

    removing duplicate files

--- ncliq.py
+++
@@ -1,9 +0,0 @@
-import webapp2
-
-class MainPage(webapp2.RequestHandler) :
-	def get(self):
-		self.response.headers['Content-Type']='text/plain'
-		self.response.write('NCLIQ !!!')
-
-app = webapp2.WSGIApplication([('/', MainPage)], debug=True)
-
